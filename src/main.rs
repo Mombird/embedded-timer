008cee86cc54f7dc668764269c02b61cee81700c
Robin Gearn <robin@wescottdesign.com>
2019-08-08 21:03:05 -0700

    clean up source, remove unnedded use statements, comment out unused time code

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,15 @@
 
 #[allow(unused_extern_crates)]
 
-// pick a panicking behavior
-// extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
 extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
 use f3::hal::delay::Delay;
 use f3::hal::prelude::*;
-use f3::hal::stm32f30x::rcc;
-use f3::hal::stm32f30x::{self, GPIOA, RCC};
+//use f3::hal::stm32f30x::rcc;
+//use f3::hal::stm32f30x::{self};
+use f3::hal::stm32f30x;
 
-use f3::hal::gpio::gpioa::{self,PA0};
+use f3::hal::gpio::gpioa::PA0;
 use f3::hal::gpio::{Input, Floating};
 use f3::led::Leds;
 use cortex_m_rt::ExceptionFrame;
@@ -32,15 +29,18 @@ use cortex_m_rt::exception;
 #[entry]
 fn main() -> ! {
     // get processor and discovery board peripherals
-    let cp = cortex_m::Peripherals::take().unwrap();
+    // not using time here
+    // let cp = cortex_m::Peripherals::take().unwrap();
     let dp = stm32f30x::Peripherals::take().unwrap();
 
-    let mut flash = dp.FLASH.constrain();
+    // not using time here
+    // let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
 
+    // not using time here
     // set up system timer
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-    let delay = Delay::new(cp.SYST, clocks);
+    // let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    // let delay = Delay::new(cp.SYST, clocks);
 
     // enable (power on) button
     let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
