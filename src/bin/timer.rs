c231e9afd58e76f9eb9a7405a0225737adf84513
Robin Gearn <robin@wescottdesign.com>
2019-08-11 10:36:34 -0700

    add James to copywright

--- src/bin/timer.rs
+++ src/bin/timer.rs
@@ -1,4 +1,4 @@
-// Copyright © 2019 Robin Gearn
+// Copyright © 2019 Robin Gearn, James Wescott
 // [This program is licensed under the "MIT License"]
 // Please see the file LICENSE in the source
 // distribution of this software for license terms.
